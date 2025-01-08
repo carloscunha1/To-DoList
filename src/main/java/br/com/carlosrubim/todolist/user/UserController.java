383c926139180546c1c5ddaede003ab930db93f1
Carlos Cunha <carlosrubim10@gmail.com>
2025-01-07 23:21:15 -0300

    Utilizando HASH para criptografia de senhas

--- src/main/java/br/com/carlosrubim/todolist/user/UserController.java
+++ src/main/java/br/com/carlosrubim/todolist/user/UserController.java
@@ -4,26 +4,25 @@ import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import at.favre.lib.crypto.bcrypt.BCrypt;
 
-/**
- * Modificador
- * public
- * private
- * protected
- */
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
     @Autowired
     private IUserRepository userRepository;
+
     @PostMapping("")
-    public ResponseEntity create(@RequestBody UserModel userModel){
+    public ResponseEntity create(@RequestBody UserModel userModel) {
         var user = this.userRepository.findByUsername(userModel.getUsername());
-            if(user != null) {
-                return ResponseEntity.status(400).body("Usuário já existe");
-            }
+        if (user != null) {
+            return ResponseEntity.status(400).body("Usuário já existe");
+        }
+        var hashedPassword = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+        userModel.setPassword(hashedPassword);
+
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(200).body(userCreated);
-           }
+    }
 }
